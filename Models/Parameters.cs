1e46cf23c87a8f3ba709f833d1c1e0fdfa497616
Prota <justfordemo0726@gmail.com>
2020-10-12 14:22:39 +0800

    first commit

---
+++ Models/Parameters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dapper_ORM.Models
+{
+    public class Parameters
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+        
+    }
+}
